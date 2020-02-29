d96ee2d41a5808bf744e6b6748cf7355e7305c8a
Anudeep Yakkala <ayakkala@purdue.edu>
2020-02-29 11:40:16 -0600

    commit from Anudeep Machine

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import CardGroup from 'react-bootstrap/CardGroup'
+
 
 export default class Navbar extends Component {
-    
+
     render() {
-        return(
+        return (
             <div>
-                <p>hello</p>
+                <Row>
+                    <CardGroup>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This card has supporting text below as a natural lead-in to additional
+                                    content.{' '}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This card has even longer content than the first to
+                                    show that equal height action.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Row>
             </div>
         )
     }
