a25f0be46d8a62e1a162f8b106de4b9cfc264ba6
Anudeep Yakkala <ayakkala@purdue.edu>
2020-02-29 12:05:22 -0600

    commit from Anudeep Machine

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -4,17 +4,16 @@ import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import CardGroup from 'react-bootstrap/CardGroup'
-
+import paintBrush from '../../public/paintbrushlogo.jpeg'
 
 export default class Navbar extends Component {
-
     render() {
         return (
             <div>
                 <Row>
                     <CardGroup>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img variant="top" src={paintBrush} />
                             <Card.Body>
                                 <Card.Title>Card title</Card.Title>
                                 <Card.Text>
@@ -24,7 +23,7 @@ export default class Navbar extends Component {
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
                             <Card.Body>
                                 <Card.Title>Card title</Card.Title>
                                 <Card.Text>
@@ -34,14 +33,73 @@ export default class Navbar extends Component {
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
                             <Card.Body>
                                 <Card.Title>Card title</Card.Title>
                                 <Card.Text>
                                     This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This card has even longer content than the first to
-                                    show that equal height action.
+                                    additional content. 
                                 </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. 
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. 
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. 
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. 
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content.
+                            </Card.Text>
                             </Card.Body>
                         </Card>
                     </CardGroup>
