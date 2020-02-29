b854638df21ef4005c86eec0940d48b3ff0b6a9d
Anudeep Yakkala <ayakkala@purdue.edu>
2020-02-29 11:20:44 -0600

    test

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import { AppBar, Toolbar, IconButton, Typography,  Button } from '@material-ui/core';
-
+import { Button } from 'react-bootstrap';
 
 export default class Navbar extends Component {
     
     render() {
         return(
-            <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                Specpres 
-                </Typography>
-                <Button color="inherit">Get Started</Button>
-            </Toolbar>
-            </AppBar>
+            <div>
+                <p>hello</p>
+            </div>
         )
     }
 }
